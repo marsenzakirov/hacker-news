02dcad20d2ed81ba0b66db8bbc2d4f5f7fe27f4b
marsen <marsenzakirov11@gmail.com>
2022-10-28 13:21:36 +0300

    Add fetchJSON function

---
+++ src/utils/fetchJSON.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+class APIError extends Error {
+  error: { code: any; message: any; data: any };
+  constructor(code: number, message: string, data: any) {
+    super(message);
+    this.error = { code: code, message: message, data: data };
+  }
+}
+interface IProps {
+  method: string;
+  data?: any;
+  headers?: any;
+}
+export async function fetchJSON(
+  url: string,
+  { method, data, headers }: IProps
+) {
+  return axios(url, { method: method, data: data, headers: headers })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+      if (e.response.status >= 500) {
+        throw new APIError(
+          e.response.status,
+          `Internal error: ${e.response.status}`,
+          null
+        );
+      }
+      if (!e.response.data?.error) {
+        throw new APIError(e.response.status, e.message, e.response.data);
+      }
+      throw new APIError(
+        e.response.status,
+        e.response.data.error.message,
+        e.response.data.error.data
+      );
+    });
+}
