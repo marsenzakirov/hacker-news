879d237b2d82f861e6e8a58b8625de0d58211068
marsen <marsenzakirov11@gmail.com>
2022-10-28 13:21:15 +0300

    Add app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Dashboard from "./Dashboard";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 function App() {
   return (
-    <div className="">
+    <div>
+      <Header />
+      <Dashboard />
+      <Footer />
     </div>
   );
 }
