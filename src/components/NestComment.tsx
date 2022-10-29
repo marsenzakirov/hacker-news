fa8c08ec9fd0c7e22d01b192c196c13bbc822945
marsen <marsenzakirov11@gmail.com>
2022-10-29 19:27:39 +0300

    Add NestComment

---
+++ src/components/NestComment.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Divider } from "react-daisyui";
+
+interface IProps {
+  text: string;
+  author: string;
+  date: string;
+}
+export default function NestComment({ text, author, date }: IProps) {
+  return (
+    <div className="m-w-4/5">
+      <h3 className="text-2xl">loh{author}</h3>
+      <p>{text}</p>
+      <p className="text-right">
+        {new Date(date).toLocaleDateString("en-gb", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+        })}
+      </p>
+      <Divider />
+    </div>
+  );
+}
