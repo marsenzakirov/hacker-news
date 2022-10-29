cf7591c45204ae6fdcb2cc5987e6f6c2ad33b3fc
marsen <marsenzakirov11@gmail.com>
2022-10-29 19:28:24 +0300

    Delete Footer from App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Dashboard from "./Dashboard";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import New from "./New";
 function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/new=:id" component={New} />
-      </Switch>
-      <Footer />
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/new=:id" component={New} />
+        </Switch>
     </Router>
   );
 }
